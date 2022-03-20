88b8860ae88fd77208db881d59941d58e327e307
sherlock59 <97972071+sherlock59@users.noreply.github.com>
2022-03-20 16:34:08 -0500

    just adding to save the progress of current work

--- midTraffick/src/midTraffick/Trafficlight.java
+++ midTraffick/src/midTraffick/Trafficlight.java
@@ -1,10 +1,40 @@
-package midTraffick;
-
-public class Trafficlight {
-
-	public static void main(String[] args) {
-		System.out.println("hi human specie");
-
+public class TrafficLight {
+	
+	private boolean red;
+	private boolean green;
+	private boolean yellow;
+	private static final String Reset = "\033[0m";
+	private static final String Red = "\033[31;7m";
+	private static final String Yellow = "\033[33;7m";
+	private static final String Green = "\033[32;7m";
+	
+	TrafficLight() {
+		this.green = false;
+		this.yellow = false;
+		this.red = true;
 	}
 
+	public boolean isRed() {
+		return red;
+	}
+
+	public void setRed(boolean red) {
+		this.red = red;
+	}
+
+	public boolean isGreen() {
+		return green;
+	}
+
+	public void setGreen(boolean green) {
+		this.green = green;
+	}
+
+	public boolean isYellow() {
+		return yellow;
+	}
+
+	public void setYellow(boolean yellow) {
+		this.yellow = yellow;
+	}
 }
